9908777798c2fb04f6b4477944e37c1e7cfa55cd
Cyl <labetowiez@aol.fr>
2021-09-27 13:58:05 +0200

    deuxieme exercice

--- form.php
+++ form.php
@@ -1,22 +1,22 @@
 <form  action="thanks.php"  method="post">
     <div>
         <label  for="nom">Nom :</label>
-        <input  type="text"  id="nom"  name="user_name">
+        <input  type="text"  id="nom"  name="user_name" required="required">
     </div>
     <div>
         <label  for="prenom">Prenom :</label>
-        <input  type="text"  id="prenom"  name="user_lastname">
+        <input  type="text"  id="prenom"  name="user_lastname" required="required">
     </div>
     <div>
         <label  for="courriel">Courriel :</label>
-        <input  type="email"  id="courriel"  name="user_email">
+        <input  type="email"  id="courriel"  name="user_email" required="required">
     </div>
     <div>
         <label for="phone">Téléphone :</label>
-        <input type="tel" id="phone" name="user_phone">
+        <input type="tel" id="phone" name="user_phone"required="required">
     </div>
     <div>
-        <select name="user_language">
+        <select name="user_language" required="required">
             <option value="">Choisissez votre language préféré</option>
             <option value="html">HTML</option>
             <option value="css">CSS</option>
--- thanks.php
+++ thanks.php
@@ -1,3 +1,70 @@
 <?php
 
-echo "Merci {$_POST['user_name']} {$_POST['user_lastname']} de nous avoir contacté à propos de {$_POST['user_language']}. Un de nos conseiller vous contactera soit à l’adresse {$_POST['user_email']} ou par téléphone au {$_POST['user_phone']} dans les plus brefs délais pour traiter votre demande : {$_POST['user_message']}";
+$nameErr = $lastnameErr = $emailErr = $languageErr = $messageErr = '';
+$name = $lastname = $email = $language = $message = '';
+$phone = $phoneErr = '';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if (empty($_POST['user_name'])) {
+        $nameErr = 'Name required';
+    } else {
+        $name = test_input($_POST['user_name']);
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+            $nameErr = '[Name] Only letters and white space allowed';
+        }
+    }
+
+    if (empty($_POST['user_lastname'])) {
+        $lastnameErr = 'Lastname required';
+    } else {
+        $lastname = test_input($_POST['user_lastname']);
+        if (!preg_match("/^[a-zA-Z-' ]*$/", $name)) {
+            $lastnameErr = '[Lastname] Only letters and white space allowed';
+        }
+    }
+
+    if (empty($_POST['user_email'])) {
+        $emailErr = 'Email required';
+    } else {
+        $email = test_input($_POST['user_email']);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $emailErr = '[Email] Invalid email format';
+        }
+    }
+
+    if (empty($_POST['user_phone'])) {
+        $phoneErr = 'phone required';
+    } else {
+        $phone = test_input($_POST['user_phone']);
+        if (!preg_match("/^((\+)33|0)[1-9](\d{2}){4}$/", $phone)) {
+            $phoneErr = '[Phone] Invalide phone format';
+        }
+    }
+
+    if (empty($_POST['user_language'])) {
+        $languageErr = 'language required';
+    } else {
+        $language = test_input($_POST['user_language']);
+    }
+
+    if (empty($_POST['user_message'])) {
+        $messageErr = 'Message required';
+    } else {
+        $message = test_input($_POST['user_message']);
+    }
+
+    if ($messageErr || $nameErr || $lastnameErr || $phoneErr || $emailErr || $languageErr) {
+        echo "$nameErr $lastnameErr $phoneErr $emailErr $languageErr $messageErr";
+    } else {
+        echo "Merci $name $lastname de nous avoir contacté à propos de $language. Un de nos conseiller vous contactera soit à l’adresse $email ou par téléphone au $phone dans les plus brefs délais pour traiter votre demande : $message";
+    }
+}
+
+function test_input($data)
+{
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+
+    return $data;
+}
